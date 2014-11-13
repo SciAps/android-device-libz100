6ec03236b3f11f6dd90f3399b8c04b2a0e1c3386
paul <psoucy@sciaps.com>
2014-11-13 14:37:21 -0500

    fix screen resolution thing

--- full_libz100.mk
+++ full_libz100.mk
@@ -146,11 +146,16 @@ PRODUCT_COPY_FILES += \
 
 #frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     
+PRODUCT_AAPT_CONFIG := normal mdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=140
 
 PRODUCT_NAME := full_libz100
 PRODUCT_DEVICE := libz100
 PRODUCT_MODEL := Sciaps LIBZ 100
+PRODUCT_MANUFACTURER := Sciaps
 
 PRODUCT_PROPERTY_OVERRIDES += \
         ro.opengles.version=131072 \
